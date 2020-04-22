d456d6d2b77cc5e90c3a9146f63d1b59dfccbce0
Kelvin Yelyen <kelvinyelyen@gmail.com>
2020-04-22 03:15:44 +0000

    Updates

---
+++ precache-manifest.1386080e02baf2abe9daaf607265d13e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44dceba369518ab999e962dda650be6d",
+    "url": "/devit-blog-app/index.html"
+  },
+  {
+    "revision": "6454414ae61c42c117ea",
+    "url": "/devit-blog-app/static/js/2.e392f618.chunk.js"
+  },
+  {
+    "revision": "40046c021c0025c71b5ec67323b92a85",
+    "url": "/devit-blog-app/static/js/2.e392f618.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "530be5098808e43fa5ee",
+    "url": "/devit-blog-app/static/js/main.289f94ef.chunk.js"
+  },
+  {
+    "revision": "5b4b7d92bb0176a2a694",
+    "url": "/devit-blog-app/static/js/runtime-main.34eb7566.js"
+  },
+  {
+    "revision": "53403f3118fad4f802bfbe9433198ca5",
+    "url": "/devit-blog-app/static/media/Bg_1.53403f31.jpg"
+  },
+  {
+    "revision": "36a6226e10c4fb463b6f79c1e116c4d4",
+    "url": "/devit-blog-app/static/media/Image_1.36a6226e.jpg"
+  }
+]);
